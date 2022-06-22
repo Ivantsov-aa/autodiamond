9ff40c5a10a4b6bedfff2574af688000fd1761db
ivantsov-aa <pr.minsk.ivantsov@gmail.com>
2022-06-22 18:13:05 +0300

    complete main page

---
+++ src/redux/store.js
@@ -0,0 +1,275 @@
+let store = {
+    headerNav: [
+        {
+            name: 'Гарантия',
+            path: '/guarantee'
+        },
+        {
+            name: 'Доставка',
+            path: '/delivery'
+        },
+        {
+            name: 'Каталог',
+            path: '/catalog'
+        },
+        {
+            name: 'Контакты',
+            path: '/contacts'
+        }
+    ],
+    footerNav: [
+        {
+            name: 'Гарантия',
+            path: '/guarantee'
+        },
+        {
+            name: 'Доставка',
+            path: '/delivery'
+        },
+        {
+            name: 'Запчасти',
+            path: '/catalog'
+        },
+        {
+            name: 'Контакты',
+            path: '/contacts'
+        }
+    ],
+    catalog: [
+        {
+            name: 'Chery',
+            model: [
+                'Amulet',
+                'Bonus',
+                'Cross',
+                'Eastar',
+                'Foralndi',
+                'SKimoM11',
+                'M12',
+                'QQ',
+                'QQ6',
+                'Tiggo',
+                'Very'
+            ]
+        },
+        {
+            name: 'Geely',
+            model: [
+                'Atlas',
+                'Emgrand 7',
+                'Emgrand GT',
+                'Emgrand X7',
+                'GS6',
+                'MK',
+                'MK Cross',
+                'Otaka',
+                'SC7',
+                'Vision'
+            ]
+        },
+        {
+            name: 'Great Wall',
+            model: [
+                'Coolbear',
+                'Cowry',
+                'Deer',
+                'Florid',
+                'Hover',
+                'Hover H3',
+                'Hover H5',
+                'Hover M2',
+                'Pegasus',
+                'Peri'
+            ]
+        },
+        {
+            name: 'Lifan',
+            model: [
+                'Breez',
+                'Cebrium',
+                'Celliya',
+                'Murman',
+                'Smily',
+                'Solano',
+                'X50',
+                'X60',
+                'X70'
+            ]
+        },
+        {
+            name: 'Vortex',
+            model: [
+                'Estina'
+            ]
+        },
+        {
+            name: 'BYD',
+            model: [
+                'F0',
+                'F3',
+                'F3R'
+            ]
+        },
+        {
+            name: 'Changan',
+            model: [
+                'CS35',
+                'Eado'
+            ]
+        },
+        {
+            name: 'HAVAL',
+            model: [
+                ''
+            ]
+        },
+        {
+            name: 'FAW',
+            model: [
+                'Besturn B50',
+                'V2',
+                'V5'
+            ]
+        }
+    ],
+    products: [
+        {
+            image: './images/default-icon-product.svg',
+            name: 'Петля для копота 1',
+            brand: 'Geely Coolray'
+        },
+        {
+            image: './images/default-icon-product.svg',
+            name: 'Петля для копота 2',
+            brand: 'Geely Coolray'
+        },
+        {
+            image: './images/default-icon-product.svg',
+            name: 'Петля для копота 3',
+            brand: 'Geely Coolray'
+        }
+    ],
+    popularProducts: [
+        {
+            image: './images/default-icon-product.svg',
+            name: 'Шестерня КПП заднего хода доп.вала (1-й сорт)',
+            sku: 'SC1701450',
+            quality: 'Оригинальная',
+            manuf: 'Great-Wall',
+            price: 1452,
+            quantity: 1
+        },
+        {
+            image: './images/default-icon-product.svg',
+            name: 'Шестерня КПП заднего хода доп.вала (1-й сорт)',
+            sku: 'SC1701450',
+            quality: 'Оригинальная',
+            manuf: 'Great-Wall',
+            price: 1452,
+            quantity: 1
+        },
+        {
+            image: './images/default-icon-product.svg',
+            name: 'Шестерня КПП заднего хода доп.вала (1-й сорт)',
+            sku: 'SC1701450',
+            quality: 'Оригинальная',
+            manuf: 'Great-Wall',
+            price: 1452,
+            quantity: 1
+        },
+        {
+            image: './images/default-icon-product.svg',
+            name: 'Шестерня КПП заднего хода доп.вала (1-й сорт)',
+            sku: 'SC1701450',
+            quality: 'Оригинальная',
+            manuf: 'Great-Wall',
+            price: 1452,
+            quantity: 1
+        },
+        {
+            image: './images/default-icon-product.svg',
+            name: 'Шестерня КПП заднего хода доп.вала (1-й сорт)',
+            sku: 'SC1701450',
+            quality: 'Оригинальная',
+            manuf: 'Great-Wall',
+            price: 1452,
+            quantity: 1
+        },
+        {
+            image: './images/default-icon-product.svg',
+            name: 'Шестерня КПП заднего хода доп.вала (1-й сорт)',
+            sku: 'SC1701450',
+            quality: 'Оригинальная',
+            manuf: 'Great-Wall',
+            price: 1452,
+            quantity: 1
+        },
+        {
+            image: './images/default-icon-product.svg',
+            name: 'Шестерня КПП заднего хода доп.вала (1-й сорт)',
+            sku: 'SC1701450',
+            quality: 'Оригинальная',
+            manuf: 'Great-Wall',
+            price: 1452,
+            quantity: 1
+        },
+        {
+            image: './images/default-icon-product.svg',
+            name: 'Шестерня КПП заднего хода доп.вала (1-й сорт)',
+            sku: 'SC1701450',
+            quality: 'Оригинальная',
+            manuf: 'Great-Wall',
+            price: 1452,
+            quantity: 1
+        },
+        {
+            image: './images/default-icon-product.svg',
+            name: 'Шестерня КПП заднего хода доп.вала (1-й сорт)',
+            sku: 'SC1701450',
+            quality: 'Оригинальная',
+            manuf: 'Great-Wall',
+            price: 1452,
+            quantity: 1
+        }
+    ],
+    news: [
+        {
+            logo: './images/logo-new.svg',
+            title: 'Какое масло выбрать для двигателя',
+            date: '30.08.2021',
+            article: 'В настоящий момент на рынке существует огромное множество моторных масел, от самых недорогих, до элитных. Каждый производитель нахваливает свой бренд...'
+        },
+        {
+            logo: './images/logo-new.svg',
+            title: 'Какое масло выбрать для двигателя',
+            date: '30.08.2021',
+            article: 'В настоящий момент на рынке существует огромное множество моторных масел, от самых недорогих, до элитных. Каждый производитель нахваливает свой бренд...'
+        },
+        {
+            logo: './images/logo-new.svg',
+            title: 'Какое масло выбрать для двигателя',
+            date: '30.08.2021',
+            article: 'В настоящий момент на рынке существует огромное множество моторных масел, от самых недорогих, до элитных. Каждый производитель нахваливает свой бренд...'
+        },
+        {
+            logo: './images/logo-new.svg',
+            title: 'Какое масло выбрать для двигателя',
+            date: '30.08.2021',
+            article: 'В настоящий момент на рынке существует огромное множество моторных масел, от самых недорогих, до элитных. Каждый производитель нахваливает свой бренд...'
+        },
+        {
+            logo: './images/logo-new.svg',
+            title: 'Какое масло выбрать для двигателя',
+            date: '30.08.2021',
+            article: 'В настоящий момент на рынке существует огромное множество моторных масел, от самых недорогих, до элитных. Каждый производитель нахваливает свой бренд...'
+        },
+        {
+            logo: './images/logo-new.svg',
+            title: 'Какое масло выбрать для двигателя',
+            date: '30.08.2021',
+            article: 'В настоящий момент на рынке существует огромное множество моторных масел, от самых недорогих, до элитных. Каждый производитель нахваливает свой бренд...'
+        }
+    ]
+};
+
+export default store;
